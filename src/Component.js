434fa1415231208179f633ec6f5b871582276450
Ryabov_Ivan <spaik39rus@gmail.com>
2021-04-02 17:42:47 +0300

    new String li

--- src/Component.js
+++ src/Component.js
@@ -5,6 +5,8 @@ const Component = () => {
                 <li>54</li>
                 <li>45</li>
                 <li>90</li>
+        <li>String</li>
+        <li>String</li>
           </ul>
 }
 
