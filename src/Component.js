c9baa753b4fc2312eddb4cdc5ef2a10f8fe3a484
Ryabov_Ivan <spaik39rus@gmail.com>
2021-04-02 17:39:28 +0300

    new li

---
+++ src/Component.js
@@ -0,0 +1,11 @@
+
+const Component = () => {
+    return <ul>
+                <li>Hello from Component</li>
+                <li>54</li>
+                <li>45</li>
+                <li>90</li>
+          </ul>
+}
+
+export default Component
